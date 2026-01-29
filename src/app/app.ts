dde1a3206bd24a513848a22e233788508f54ad77
Mohamed Ashraf <mohamedashraf20191700504@gmail.com>
2026-01-29 14:22:49 +0200

    create task component

--- src/app/app.ts
+++ src/app/app.ts
@@ -3,10 +3,11 @@ import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from '../header/header';
 import { User } from "./user/user";
 import { DUMMY_USERS } from './dummy-users';
+import { Task } from "./task/task";
 
 @Component({
   selector: 'app-root',
-  imports: [HeaderComponent, User],
+  imports: [HeaderComponent, User, Task],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
